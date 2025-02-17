7a8d9da51372f88062ec090a3acefdec9a301e8c
bojadlabalaji <bojadlabalaji@gmail.com>
2025-02-16 20:40:26 -0500

    deployemnt configs updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Cyber-Intellect/', // Add this line
   server: {
     port: 5000, // Specify the port here
     proxy: {
